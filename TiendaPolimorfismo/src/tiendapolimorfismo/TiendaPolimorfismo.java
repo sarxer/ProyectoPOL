c1664aeb03a350b1cff555ecee3f5e6c4c8c0bc6
Elizabeth <sear324796@gmail.com>
2026-08-28 19:35:27 -0500

    6 Crear objetos de la tienda

--- TiendaPolimorfismo/src/tiendapolimorfismo/TiendaPolimorfismo.java
+++ TiendaPolimorfismo/src/tiendapolimorfismo/TiendaPolimorfismo.java
@@ -6,6 +6,15 @@ public class TiendaPolimorfismo {
    
     public static void main(String[] args) {
          
+
+    Trabajador trabajador1 = new Trabajador("Carlos", 1001234567, "Vendedor", 1800000);
+    
+    Gerente gerente1 = new Gerente("Laura", 1009876543, "Administracion", 3000000);
+    
+    Cliente cliente1 = new Cliente("Sofia", 1012345678, "3001234567", "sofia2000@gmail.com");
+    
+    Mascota mascota1 = new Mascota("Luna", "Perro", "Labrador", 3);
+
+}
     }
     
-}
